08daf42d6af68bbc2e6b133e0dbacd07912e3d8e
HY CHOI <chlghdud17@gmail.com>
2022-05-10 12:59:50 +0900

    env add

--- nate_on/src/common/common_config.ts
+++ nate_on/src/common/common_config.ts
@@ -1,4 +1,7 @@
-export const serviceKey = '';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+export const serviceKey = process.env.serviceKey || ''
 export const weatherApi = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
 export const nx = '60'
 export const ny = '126'
