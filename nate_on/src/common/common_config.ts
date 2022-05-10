579ba58af37830a0be90454d9a328e70a5bb46bd
2Ruk <leerukdev@gmail.com>
2022-05-10 12:57:07 +0900

    express add author add

--- nate_on/src/common/common_config.ts
+++ nate_on/src/common/common_config.ts
@@ -1,4 +1,4 @@
-export const serviceKey = ''
+export const serviceKey = '';
 export const weatherApi = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
 export const nx = '60'
 export const ny = '126'
